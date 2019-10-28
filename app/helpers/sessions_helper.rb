a42788763ee904af58e18a65928b60e1ddf1eaed
Polvs <alvaroalamillovargas@gmail.com>
2019-10-28 16:48:57 +0000

    Finish user edit, update, idex and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,11 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+  
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -39,5 +44,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
