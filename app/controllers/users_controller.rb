9acda5180a80f21bf5c6c0278061610d3791569c
Polvs <alvaroalamillovargas@gmail.com>
2019-10-31 11:49:25 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -47,26 +48,21 @@ class UsersController < ApplicationController
   end
   
   private
-  
+
     def user_params
-      params.require(:user).permit(:name, :email, :password, 
-                                   :password_confimation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-    
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
+
+    # Before filters
+
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-    
+
+    # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
